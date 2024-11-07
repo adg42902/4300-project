6c0fa4b95da346486225ea638b40367c302cdef9
andreigaz <agazinschi@gmail.com>
2024-11-07 13:09:36 -0500

    feq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-8">Welcome to Spotify Stats</h1>
         <Link
-          href="/routes/login"
+          href="/account/login"
           className="px-4 py-2 mr-5 border border-green-400 bg-green-400 rounded-lg hover:bg-green-500 transition ease-in-out hover:border-green-500 duration-200 hover:scale-105 hover:shadow-md inline-block"
         >
           Go to Login
         </Link>
         <Link
-          href="/routes/signup"
+          href="/account/signup"
           className="px-4 py-2 ml-5 border border-green-400 bg-green-400 rounded-lg hover:bg-green-500 transition ease-in-out hover:border-green-500 duration-200 hover:scale-105 hover:shadow-md inline-block"
         >
           Go to Sign Up
