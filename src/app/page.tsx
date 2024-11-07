b6f9036f4ec43524927ec3233353cdef954a81d2
andreigaz <agazinschi@gmail.com>
2024-11-07 08:28:32 -0500

    fixed button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Login from "./login/page";
 import Link from "next/link";
 
 export default function Home() {
@@ -7,9 +6,9 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-8">Welcome to Spotify Stats</h1>
-        <Link 
+        <Link
           href="/login"
-          className="px-4 py-2 bg-green-400 rounded-lg hover:bg-green-500 transition"
+          className="px-4 py-2 border border-green-400 bg-green-400 rounded-lg hover:bg-green-500 transition ease-in-out hover:border-green-500 duration-200 hover:scale-105 hover:shadow-md inline-block"
         >
           Go to Login
         </Link>
