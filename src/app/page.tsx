25ec601362b64104c2aebfc139d78265764fc469
andreigaz <agazinschi@gmail.com>
2024-11-07 13:02:48 -0500

    added frontend parts of initial landing page, signup, and login
    \n  -Need to integrate authentication with database for login.\n  -Need to add push to database for account creation\n . -Need to make dashboard dummy view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,17 @@ export default function Home() {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-8">Welcome to Spotify Stats</h1>
         <Link
-          href="/login"
-          className="px-4 py-2 border border-green-400 bg-green-400 rounded-lg hover:bg-green-500 transition ease-in-out hover:border-green-500 duration-200 hover:scale-105 hover:shadow-md inline-block"
+          href="/routes/login"
+          className="px-4 py-2 mr-5 border border-green-400 bg-green-400 rounded-lg hover:bg-green-500 transition ease-in-out hover:border-green-500 duration-200 hover:scale-105 hover:shadow-md inline-block"
         >
           Go to Login
         </Link>
-        {/*Also include a create account*/}
+        <Link
+          href="/routes/signup"
+          className="px-4 py-2 ml-5 border border-green-400 bg-green-400 rounded-lg hover:bg-green-500 transition ease-in-out hover:border-green-500 duration-200 hover:scale-105 hover:shadow-md inline-block"
+        >
+          Go to Sign Up
+        </Link>
       </div>
     </div>
   );
---
+++ src/app/routes/login/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import React from "react";
+
+export default function Login() {
+  const dummyUsername = "test";
+  const dummyPassword = "password";
+
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [buttonText, setButtonText] = useState("Login");
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const loginDisabled = isLoading || !formData.username || !formData.password;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    setButtonText("Logging in...");
+    setLoading(true);
+    e.preventDefault();
+    // Implement Database logic here with get to check username/password
+    // Dummy data for now
+
+    if (
+      formData.username === dummyUsername &&
+      formData.password === dummyPassword
+    ) {
+      /* Route to dashboard view when implemented
+        router.push("/dashboard");
+        router.refresh();
+        */
+      setButtonText("Login Success");
+    } else {
+      setError("Incorrect username or password");
+      setButtonText("Login");
+    }
+  };
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-center text-3xl font-bold my-8">
+          Spotify
+          <br /> Stats
+        </h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col space-y-4 w-full max-w-md pd-8"
+        >
+          <div className="grid grid-cols-2 gap-4">
+            <label className="text-right p-1" htmlFor="username">
+              Login
+            </label>
+            <input
+              className="p-1 border rounded-md transition bg-gray-200 duration-200 ease-in-out focus:bg-white"
+              type="text"
+              placeholder="username"
+              id="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            ></input>
+            <label className="text-right p-1" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="p-1 border rounded-md transition bg-gray-200 duration-200 ease-in-out focus:bg-white"
+              type="password"
+              placeholder="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              disabled={loginDisabled}
+              className="border rounded-lg py-1 px-4 bg-green-400 border-green-400 transition ease-in-out duration-200 hover:bg-green-500 hover:scale-105 hover:shadow-md disabled:bg-gray-300 disabled:border-gray-300 disabled:hover:scale-100 disabled:opacity-50 disabled:hover:shadow-none"
+              type="submit"
+            >
+              {buttonText}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
